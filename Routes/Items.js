74a035e3f8fa372844612be7303992473229ecd3
calt08 <cesarlopez2808@gmail.com>
2020-09-04 01:15:29 -0400

    Filter and Patch working

--- Routes/Items.js
+++ Routes/Items.js
@@ -1,12 +1,35 @@
 const router = require("express").Router();
-const { ItemSchema } = require("../Schemas/Items");
-// const Joi = require("joi");
+const { ItemSchema, ItemPatchSchema } = require("../Schemas/Items");
 const { Item } = require("../models");
 
 router.get("", async (req, res) => {
-    const items = await Item.findAll();
+
+    let items = await Item.findAll();
+
+    if (req.query.startDate) {
+        startDate = new Date(req.query.startDate);
+        items = items.filter(item => item.dueDate > startDate);
+    }
+    if (req.query.endDate) {
+        endDate = new Date(req.query.endDate);
+        items = items.filter(item => item.dueDate < endDate);
+    }
+    if (req.query.status) {
+        items = items.filter(item => item.status == JSON.parse(req.query.status));
+    }
 
     res.status(200).send(items);
+
+
+});
+
+router.get("/test/:id", async (req, res) => {
+
+    const items = await Item.findAll();
+
+    let item = items.find((elem) => elem.id == req.params.id)
+
+    res.status(200).send(item);
 });
 
 router.post("", async (req, res) => {
@@ -29,7 +52,7 @@ router.put("/:id", (req, res) => {
     }
 
     let item = itemsList.find((elem) => elem.id == req.params.id);
-    item.owner = req.body.owner;
+    // item.owner = req.body.owner;
     item.description = req.body.description;
     item.status = req.body.status;
     item.dueDate = req.body.dueDate;
@@ -37,5 +60,49 @@ router.put("/:id", (req, res) => {
     res.status(200).send(item);
 
 })
+
+router.patch("/:id", async (req, res) => {
+    const validation = ItemPatchSchema.validate(req.body);
+    if (validation.error) {
+        res.status(400).send(validation);
+    }
+
+    let itemSelected = await Item.findByPk(req.params.id);
+
+    if (itemSelected === null) {
+        res.status(404).send("Not found")
+    }
+
+    // for (const element in req.body) {
+    //     console.log(`${element}`)
+    //     if (req.body.hasOwnProperty(element)) {
+    //         itemSelected.update({
+    //             "element": req.body[element]
+    //         })
+    //     }
+    // }
+
+    if (validation.value.description) {
+        itemSelected.update({
+            "description": validation.value.description
+        })
+    }
+    if (validation.value.status) {
+        itemSelected.update({
+            "status": validation.value.status
+        })
+    }
+    if (validation.value.dueDate) {
+        itemSelected.update({
+            "dueDate": validation.value.dueDate
+        })
+    }
+
+    res.status(200).send(itemSelected)
+
+});
+
+
+
 
 module.exports = router;
--- Schemas/Items.js
+++ Schemas/Items.js
@@ -1,12 +1,18 @@
 const Joi = require("joi");
 
 const ItemSchema = Joi.object().keys({
-    // id: Joi.number().required(),
     ownerId: Joi.number().positive().required(),
     description: Joi.string().required(),
     status: Joi.bool().required(),
     dueDate: Joi.date().required()
 })
 
+const ItemPatchSchema = Joi.object().keys({
+    description: Joi.string(),
+    status: Joi.bool(),
+    dueDate: Joi.date()
+})
 
-module.exports = { ItemSchema };
+
+
+module.exports = { ItemSchema, ItemPatchSchema };
