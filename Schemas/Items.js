cd697c21c86a9cf5b4bb9f94eb7fb99849fb98b0
calt08 <cesarlopez2808@gmail.com>
2020-09-03 20:30:08 -0400

    Non positive OwnerID

--- Schemas/Items.js
+++ Schemas/Items.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const ItemSchema = Joi.object().keys({
     // id: Joi.number().required(),
-    ownerId: Joi.number().required(),
+    ownerId: Joi.number().positive().required(),
     description: Joi.string().required(),
     status: Joi.bool().required(),
     dueDate: Joi.date().required()
